a897c839a179be3824e06f8b285a6e3ea8113bdf
ruanbrito <rpablobrito@gmail.com>
2021-11-21 23:15:15 -0300

    criada o pacote chess e sua classes

--- src/aplication/Program.java
+++ src/aplication/Program.java
@@ -1,9 +1,16 @@
 package aplication;
 
+import chess.ChessMatch;
+import chess.ChessPiece;
+
 public class Program {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		ChessMatch chessMatch = new ChessMatch();
+		
+		UI.printBoard(chessMatch.getPieces());
 
 	}
 
